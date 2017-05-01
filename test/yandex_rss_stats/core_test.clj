eb17c41df41b03b392b941da35b75a05f5209d84
j <6665840@gmail.com>
2017-05-01 18:07:37 +0600

    add coverage for routing

--- test/yandex_rss_stats/core_test.clj
+++ test/yandex_rss_stats/core_test.clj
@@ -1,7 +1,27 @@
 (ns yandex-rss-stats.core-test
   (:require [clojure.test :refer :all]
-            [yandex-rss-stats.core :refer :all]))
+            [yandex-rss-stats.core :refer :all]
+            [org.httpkit.client :as http]))
+
+(defn with-server-started [f]
+  (with-redefs [yandex-rss-stats.controller/search (fn [req] {:status 200
+                                                              :body "Mock response body"})]
+    (start-server)
+    (try
+      (f)
+      (finally
+        (stop-server)))))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "/search"
+    (is (= "Mock response body"
+           (:body @(http/get "http://localhost:8080/search" {:as :text})))))
+  (testing "404"
+    (is (= 404
+           (:status @(http/get "http://localhost:8080/foobar"))))))
+
+(use-fixtures :each with-server-started)
+
+(comment
+  (run-tests)
+  )
