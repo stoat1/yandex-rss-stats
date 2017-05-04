72f962fd724a4259741b8c4421a62878fdc8eb2e
j <6665840@gmail.com>
2017-05-05 00:51:06 +0600

    test server with async handler

--- test/yandex_rss_stats/core_test.clj
+++ test/yandex_rss_stats/core_test.clj
@@ -1,22 +1,26 @@
 (ns yandex-rss-stats.core-test
   (:require [clojure.test :refer :all]
             [yandex-rss-stats.core :refer :all]
+            [org.httpkit.server    :refer [with-channel send!]]
             [org.httpkit.client :as http]))
 
 (defn with-server-started [f]
-  (with-redefs [yandex-rss-stats.controller/handler (fn [req] {:status 200
-                                                               :body (:body req)})]
-    (start-server)
-    (try
-      (f)
-      (finally
-        (stop-server)))))
+  (letfn [(echo-async-handler [{:keys [async-channel body]}]
+            (send! async-channel {:status 200
+                                  :body   body})
+            {:body async-channel})]
+    (with-redefs [yandex-rss-stats.controller/handler echo-async-handler]
+      (start-server)
+      (try
+        (f)
+        (finally
+          (stop-server))))))
 
+;; test that server supports async handlers
 (deftest a-test
   (testing "/search"
     (is (= "ping"
            (:body @(http/post "http://localhost:8080/foo/bar" {:body "ping"
-                                                              :as :text}))))))
-
+                                                               :as   :text}))))))
 
 (use-fixtures :each with-server-started)
